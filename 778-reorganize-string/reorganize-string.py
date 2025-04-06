a4d87caed545a6f82b96dc242bcc2e042c34f666
Nguyen Khoi Nguyen <nnguyen11296@gmail.com>
2025-04-05 20:25:29 -0700

    Time: 11 ms (5.06%) | Memory: 17.7 MB (75.09%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.py
@@ -0,0 +1,24 @@
+from collections import Counter
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        counter = Counter(s)
+        result = ''
+        for _ in range(len(s)):
+            if len(counter) == 1:
+                letter, count = list(counter.items())[0]
+                if count == 1:
+                    return result + letter
+                else:
+                    return ""
+            top_2_common_letters = counter.most_common(2)
+            if len(result) != 0 and result[-1] == top_2_common_letters[0][0]:
+                letter = top_2_common_letters[1][0]
+            else:
+                letter = top_2_common_letters[0][0]
+            result += letter
+            counter[letter] -= 1
+            if counter[letter] == 0:
+                del counter[letter]
+        return result
+
+        
