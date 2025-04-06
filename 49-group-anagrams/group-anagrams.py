0d2b67fdf230fb81111ff1db76a38458029bdac1
Nguyen Khoi Nguyen <nnguyen11296@gmail.com>
2025-04-05 18:07:22 -0700

    Time: 21 ms (24.02%) | Memory: 22.7 MB (12.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,12 @@
 from collections import defaultdict
 
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        word_set_list = []
-        result = []
+        groupings = defaultdict(list)
         for word in strs:
-            sorted_word = sorted(list(word))
-            match = False
-            for i, check_sorted_word in enumerate(word_set_list):
-                if sorted_word == check_sorted_word:
-                    match = True
-                    result[i].append(word)
-                    break
-            if not match:
-                word_set_list.append(sorted_word)
-                result.append([word])
-        return result
-    
-    #optimal: prime multiplication (assign prime for each letter of the word)
-        
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord("a")] += 1
+            groupings[tuple(count)].append(word)
+        return list(groupings.values())
