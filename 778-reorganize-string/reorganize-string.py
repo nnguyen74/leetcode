f2d2532a06d8c2bbb897f52ca85c1157d5bcc926
Nguyen Khoi Nguyen <nnguyen11296@gmail.com>
2025-04-05 20:54:31 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (75.09%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,24 +1,24 @@
-from collections import Counter
+from heapq import heappush, heappop
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s)
+        pq = [(-count, char) for char, count in Counter(s).items()]
+        heapify(pq)
         result = ''
-        for _ in range(len(s)):
-            if len(counter) == 1:
-                letter, count = counter.most_common(1)[0]
-                if count == 1:
-                    return result + letter
-                else:
-                    return ""
-            top_2_common_letters = counter.most_common(2)
-            if len(result) != 0 and result[-1] == top_2_common_letters[0][0]:
-                letter = top_2_common_letters[1][0]
+        while pq:
+            count, char = heappop(pq)
+            if len(result) == 0 or result[-1] != char:
+                result += char
+                if count != -1:
+                    heappush(pq, (count + 1, char))
             else:
-                letter = top_2_common_letters[0][0]
-            result += letter
-            counter[letter] -= 1
-            if counter[letter] == 0:
-                del counter[letter]
+                if not pq:
+                    return ""
+                count2, char2 = heappop(pq)
+                result += char2
+                if count2 != -1:
+                    heappush(pq, (count2 + 1, char2))
+                heappush(pq, (count, char))
         return result
+        
 
         
